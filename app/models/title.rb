7bac5a45a1c242e4322050c76147d785b782211d
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-11 22:17:37 +0200

    rails generate model Video video_title:string youtube_link:string title:references

--- app/models/title.rb
+++ app/models/title.rb
@@ -2,6 +2,7 @@ class Title < ApplicationRecord
   has_many :ratings
   has_many :users, through: :ratings
   has_many :images
+  has_many :videos
 
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :awards
