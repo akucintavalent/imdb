d44bcf1e9ee9a6b921905d9a1dc4d5f5a4c380c2
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-12 18:58:54 +0200

    rails generate controller Companies index show new edit create update destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'companies/index'
+  get 'companies/show'
+  get 'companies/new'
+  get 'companies/edit'
+  get 'companies/create'
+  get 'companies/update'
+  get 'companies/destroy'
   get 'user/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
