6c72d44b42b1db8d805bb7056aa9a0d10260e4d0
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-12 11:14:39 +0200

    Replace title::has_many :users, through: :ratings|

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,6 +1,4 @@
 class Title < ApplicationRecord
-  has_many :ratings
-  has_many :users, through: :ratings
   has_many :images
   has_many :videos
   has_many :stars
@@ -8,6 +6,8 @@ class Title < ApplicationRecord
   has_many :writers
   has_many :directors
 
+  has_many :ratings
+  has_many :users, through: :ratings
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :awards
   has_and_belongs_to_many :companies
