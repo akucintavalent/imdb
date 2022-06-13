e0696d6144c38c8530c611889122d7b20cb2f3f8
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-13 13:09:19 +0200

    Implement edit view and update action in companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -12,6 +12,7 @@ class CompaniesController < ApplicationController
   end
 
   def edit
+    @company = Company.find(params[:id])
   end
 
   def create
@@ -29,6 +30,16 @@ class CompaniesController < ApplicationController
   end
 
   def update
+    @company = Company.find(params[:id])
+    respond_to do |format|
+      if @company.update(company_params)
+        format.html { redirect_to @company, notice: 'Company was successfully updated.' }
+        format.json { render :show, status: :ok, location: @company }
+      else
+        format.html { render :edit }
+        format.json { render json: @company.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
