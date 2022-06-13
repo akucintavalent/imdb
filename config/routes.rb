d1ce814f71c6acd1f13d56f19cd9761fcf348343
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-13 12:15:59 +0200

    Change routes.rb file to contain resources :companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  get 'companies/index'
-  get 'companies/show'
-  get 'companies/new'
-  get 'companies/edit'
-  get 'companies/create'
-  get 'companies/update'
-  get 'companies/destroy'
   get 'user/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :companies
   # Defines the root path route ("/")
   root "home#index"
 end
