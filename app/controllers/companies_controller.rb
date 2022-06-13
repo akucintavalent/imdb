0bd69cf74aeec0a5e78f62deca4a32d0e68fe0e0
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-13 12:57:22 +0200

    Edit create action from companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -16,6 +16,16 @@ class CompaniesController < ApplicationController
 
   def create
     @company = Company.new(company_params)
+
+    respond_to do |format|
+      if @company.save
+        format.html { redirect_to @company, notice: 'Company was successfully created.' }
+        format.json { render :show, status: :created, location: @company }
+      else
+        format.html { render :new }
+        format.json { render json: @company.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
