53c340b2210f6a6c5f81a4e78f8f4fdeb5f77e4d
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-06-13 12:49:19 +0200

    Adjust actions show, new, create and views show and new

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,20 +4,29 @@ class CompaniesController < ApplicationController
   end
 
   def show
+    @company = Company.find(params[:id])
   end
 
   def new
+    @company = Company.new
   end
 
   def edit
   end
 
   def create
+    @company = Company.new(company_params)
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name)
   end
 end
